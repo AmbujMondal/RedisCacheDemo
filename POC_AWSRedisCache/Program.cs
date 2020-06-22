61c815b7e046048a9affdd756da85831f4f67c35
Ambuj Mondal <amondal@ep.com>
2020-06-22 12:24:24 +0530

    changes

---
+++ POC_AWSRedisCache/Program.cs
@@ -0,0 +1,74 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace POC_AWSRedisCache
+{
+    class Program
+    {
+        readonly int devicesCount = 5;
+        IDatabase cache = RedisConnectionHelper.Connection.GetDatabase();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing Redis Cache.");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("AWS- AmbujRedisDB");
+            Console.WriteLine("Endpoint: redis - 13990.c212.ap - south - 1 - 1.ec2.cloud.redislabs.com:13990");
+            Console.WriteLine("------------------------------------");
+            var program = new Program();
+            Console.WriteLine();
+            Console.WriteLine("Saving random data in cache");
+            Console.WriteLine("------------------------------------");
+            program.SaveBigData();
+            Console.WriteLine();
+            Console.WriteLine("Reading data from cache");
+            Console.WriteLine("------------------------------------");
+            program.ReadData();
+            Console.WriteLine();
+            Console.WriteLine("Clearing data from cache");
+            Console.WriteLine("------------------------------------");
+            program.ClearCache();
+
+            Console.ReadLine();
+        }
+
+        public void ReadData()
+        {
+            for (int i = 1; i < devicesCount; i++)
+            {
+                var value = cache.StringGet($"Device_Status:{i}");
+                Console.WriteLine($"Device_Status:{i} ,Value={value}");
+            }
+            Console.WriteLine("------------------------------------");
+        }
+
+        public void SaveBigData()
+        {
+            
+            var rnd = new Random();
+
+            for (int i = 1; i < devicesCount; i++)
+            {
+                var value = rnd.Next(0, 5);
+                Console.WriteLine($"Device_Status:{i} : {value}");
+                cache.StringSet($"Device_Status:{i}", value);
+            }
+            Console.WriteLine("------------------------------------");
+        }
+        public void ClearCache()
+        {
+            
+            int j = 0;
+            for (int i = 1; i < devicesCount; i++)
+            {
+                if (cache.KeyDelete($"Device_Status:{i}"))
+                {
+                    j++;
+                    Console.WriteLine($"Deleted Device_Status:{i}.");
+                }
+            }
+            Console.WriteLine($"Deleted {j} keys.");
+            Console.WriteLine("------------------------------------");
+        }
+    }
+}
---
+++ POC_AWSRedisCache/RedisConnectionHelper.cs
@@ -0,0 +1,35 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace POC_AWSRedisCache
+{
+    public class RedisConnectionHelper
+    {
+        static readonly ConfigurationOptions options = new ConfigurationOptions
+        {//"redis-13990.c212.ap-south-1-1.ec2.cloud.redislabs.com:13990" 
+            EndPoints = { "smartpo-redis-dev.q0mgbe.ng.0001.usw2.cache.amazonaws.com:6379" },
+            //Password = "ambujRedis123",
+            Ssl = false,
+            AbortOnConnectFail = false,
+            DefaultDatabase = 0
+            
+        };
+        static RedisConnectionHelper()
+        {
+            lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+            {
+                return ConnectionMultiplexer.Connect(options);
+            });
+        }
+
+        private static readonly Lazy<ConnectionMultiplexer> lazyConnection;
+
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return lazyConnection.Value;
+            }
+        }
+    }
+}
